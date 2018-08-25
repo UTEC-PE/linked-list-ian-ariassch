2816b2b783b50d25e2c3a16807da031624b33044
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2018-08-24 19:46:22 -0500

    Popback, popfront y print

--- list.h
+++ list.h
@@ -12,20 +12,27 @@ class List {
     private:
         Node<T>* head;
         Node<T>* tail;
-        int nodes;
+
 
         void print_reverse(Node<T>* head);
 
     public:
+        int nodes;
         List(){
             head = NULL;
             tail = NULL;
         };
 
         T front(){
+            if(!head){
+                throw "322";
+            }
             return head->data;
         };
         T back(){
+            if(!head){
+                throw "322";
+            }
             return tail->data;
         };
         void push_front(T value){
@@ -38,9 +45,10 @@ class List {
                 tail=temp;
                 temp=NULL;
             }
-            else
+            else{
             temp->next=head;
-            head=temp;
+            head=temp;}
+            nodes++;
         };
         void push_back(T value){
             Node<T> *temp=new Node<T>;
@@ -57,14 +65,55 @@ class List {
                 tail->next=temp;
                 tail=temp;
             }
+            nodes++;
         };
-        void pop_front();
-        void pop_back();
+        void pop_front(){
+            Node<T> *temp = head;
+            if(!head){
+                throw "Lista Vacia";
+            }
+            else if(nodes == 1){
+                delete head;
+            }
+            else{
+            head = temp->next;
+
+            }
+            delete temp;
+            nodes--;
+        };
+        void pop_back(){
+            auto *temp = head;
+            if(!head){
+                throw "Lista Vacia";
+            }
+            if(nodes == 1){
+                delete head;
+            }
+            else
+            {
+            while(temp->next != tail){
+                temp = temp->next;
+            }
+
+            tail = temp;
+            temp = temp->next;
+            }
+            nodes--;
+            delete temp;
+        };
         T get(int position);
         void concat(List<T> &other);
         bool empty();
         int size();
-        void print();
+        void print(){
+            Node<T> *temp = head;
+            while(temp != tail){
+                cout<< temp->data<<" ";
+                temp = temp->next;
+            }
+            cout<<temp->data<<" ";
+        };
         void print_reverse();
         void clear();
         Iterator<T> begin();
