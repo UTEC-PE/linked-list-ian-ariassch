af361689eb03e95f5eb69e346612a370a9e074ce
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2018-08-28 16:15:33 -0500

    Add files via upload

--- list.h
+++ list.h
@@ -9,116 +9,148 @@ using namespace std;
 
 template <typename T>
 class List {
-    private:
-        Node<T>* head;
-        Node<T>* tail;
+private:
+    Node<T>* head;
+    Node<T>* tail;
 
 
-        void print_reverse(Node<T>* head);
+    void print_reverse(Node<T>* head);
 
-    public:
-        int nodes;
-        List(){
-            head = NULL;
-            tail = NULL;
-        };
+public:
+    int nodes;
+    List(){
+        head = NULL;
+        tail = NULL;
+    };
 
-        T front(){
-            if(!head){
-                throw "322";
-            }
-            return head->data;
-        };
-        T back(){
-            if(!head){
-                throw "322";
-            }
-            return tail->data;
-        };
-        void push_front(T value){
-            Node<T> *temp=new Node<T>;
-            temp->data=value;
-            temp->next=NULL;
-            if(head==NULL)
-            {
-                head=temp;
-                tail=temp;
-                temp=NULL;
-            }
-            else{
+    T front(){
+        if(!head){
+            throw "322";
+        }
+        return head->data;
+    };
+    T back(){
+        if(!head){
+            throw "322";
+        }
+        return tail->data;
+    };
+    void push_front(T value){
+        Node<T> *temp=new Node<T>;
+        temp->data=value;
+        temp->next=NULL;
+        if(head==NULL)
+        {
+            head=temp;
+            tail=temp;
+            temp=NULL;
+        }
+        else{
             temp->next=head;
             head=temp;}
-            nodes++;
-        };
-        void push_back(T value){
-            Node<T> *temp=new Node<T>;
-            temp->data=value;
-            temp->next=NULL;
-            if(head==NULL)
-            {
-                head=temp;
-                tail=temp;
-                temp=NULL;
-            }
-            else
-            {
-                tail->next=temp;
-                tail=temp;
-            }
-            nodes++;
-        };
-        void pop_front(){
-            Node<T> *temp = head;
-            if(!head){
-                throw "Lista Vacia";
-            }
-            else if(nodes == 1){
-                delete head;
-            }
-            else{
+        nodes++;
+    };
+    void push_back(T value){
+        Node<T> *temp=new Node<T>;
+        temp->data=value;
+        temp->next=NULL;
+        if(head==NULL)
+        {
+            head=temp;
+            tail=temp;
+            temp=NULL;
+        }
+        else
+        {
+            tail->next=temp;
+            tail=temp;
+        }
+        nodes++;
+    };
+    void pop_front(){
+        Node<T> *temp = head;
+        if(!head){
+            throw "Lista Vacia";
+        }
+        else if(nodes == 1){
+            delete head;
+        }
+        else{
             head = temp->next;
 
-            }
-            delete temp;
-            nodes--;
-        };
-        void pop_back(){
-            auto *temp = head;
-            if(!head){
-                throw "Lista Vacia";
-            }
-            if(nodes == 1){
-                delete head;
-            }
-            else
-            {
+        }
+        delete temp;
+        nodes--;
+    };
+    void pop_back(){
+        auto *temp = head;
+        if(!head){
+            throw "Lista Vacia";
+        }
+        if(nodes == 1){
+            delete head;
+        }
+        else
+        {
             while(temp->next != tail){
                 temp = temp->next;
             }
 
             tail = temp;
             temp = temp->next;
-            }
-            nodes--;
-            delete temp;
-        };
-        T get(int position);
-        void concat(List<T> &other);
-        bool empty();
-        int size();
-        void print(){
-            Node<T> *temp = head;
-            while(temp != tail){
-                cout<< temp->data<<" ";
-                temp = temp->next;
-            }
-            cout<<temp->data<<" ";
-        };
-        void print_reverse();
-        void clear();
-        Iterator<T> begin();
-        Iterator<T> end();
+        }
+        nodes--;
+        delete temp;
+    };
+    T get(int position){
+        try{
+            if(position <= nodes-1){
+                if(position==0) {
+                    return head->data;
+                }
+              else if(position == nodes-1){
+                return tail->data;}
+                else{ auto temp = head;
+                while (position--){temp=temp->next;}
+                return temp->data;}}
+            else{throw position;}
+        }catch(int position){
+            cout<<"\nNo existe indice "<<position<<"\n";
+        }
+    };
+    void concat(List<T> &other);
+    bool empty(){
+        if(nodes > 0)return false;
+        else return true;
+    };
+    int size(){
+        return nodes;
+    };
+    void print(){
+        Node<T> *temp = head;
+        while(temp != tail){
+            cout<< temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<temp->data<<" ";
+    };
+    void print_reverse(){Node<T> *temp = tail;
+        while(temp != head){
+            cout<< temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<temp->data<<" ";}
+    void clear(){
+        Node<T> *temp = head;
+        while(temp != tail){
+            temp->data = NULL;
+            temp = temp->next;
+        }
+        temp->data=NULL;
+    };
+    Iterator<T> begin();
+    Iterator<T> end();
 
-        ~List();
+    ~List();
 };
 #endif
